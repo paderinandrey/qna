62fdd0cc298271c579916039fb41ea06c2220118
paderinandrey <=>
2016-08-09 18:29:00 +0000

    fixed #1

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ApplicationRecord
-  belongs_to :question
+  belongs_to :question, touch: true
   
   validates :body, :question_id, presence: true
 end
--- db/migrate/20160808212258_create_answers.rb
+++ db/migrate/20160808212258_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
       t.text :body
-      t.references :question, index: true
+      t.references :question, index: true, foreign_key: true
 
       t.timestamps
     end
